6a589660c6a1047b09f522d7841cb8c0f855b063
Cor Zuurmond <jczuurmond@protonmail.com>
2021-02-26 09:58:54 +0100

    Add plain main to terraform folder

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
